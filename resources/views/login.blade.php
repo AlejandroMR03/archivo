36846a3500a5aa65464bf7618fac1c6aa089f89e
Miguel Agudelo <agudelomiguel04@gmail.com>
2023-03-06 14:45:48 -0500

    actualizar app

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="app.css" />
+    <title>Ingreso</title>
+</head>
+<body>
+<img src="fondo.jpg" class="rounded mx-auto d-block" alt="...">
+<br>
+<p class="fw-bolder text-center fs-1">ARCHIVO</p>
+<div class="wrapper">
+    <form class="form-signin">       
+    <p class="fst-italic text-center fs-4">Inicia Sesion</p>
+      <br>
+      <input type="text" class="form-control" name="username" placeholder="Correo Electronico" required="" autofocus="" />
+      <br>
+      <input type="password" class="form-control" name="password" placeholder="Contraseña" required=""/>      
+      <br>
+      <center><button type="submit" class="btn btn-success">Ingresar</button></center>
+    </form>
+  </div>
+</body>
+</html>
